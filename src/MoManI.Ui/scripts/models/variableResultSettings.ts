a40b70f69571bb0b2aa118b572c828b7244307a2
Martynas Pelakauskas <martynas@ut.ee>
2016-08-02 23:30:07 +0300

    set data groups for result visualizations

--- src/MoManI.Ui/scripts/models/variableResultSettings.ts
+++ src/MoManI.Ui/scripts/models/variableResultSettings.ts
@@ -1,12 +1,41 @@
 ﻿import _ = require('lodash');
 
+export class SetDataGroup {
+    id: string;
+    name: string;
+    items: ISelectableEntry<ISetDataItem>[];
+    selected: boolean;
+    toggleGroupItems: (event) => void;
+    selectedItems: () => ISetDataItem[];
+
+    constructor(id: string, name: string, items: ISetDataItem[], changeHandler: () => void) {
+        this.id = id;
+        this.name = name;
+        this.items = _.map(items, (i: ISetDataItem) => {
+            return {
+                data: i,
+                selected: true,
+            };
+        });
+        this.selected = true;
+        this.toggleGroupItems = (checked) => {
+            var checkbox = <ICheckbox>checked.target;
+            _.forEach(this.items, i => { i.selected = checkbox.checked });
+            changeHandler();
+        }
+        this.selectedItems = () => {
+            return _(this.items).filter(i => i.selected).map(i => i.data).value();
+        }
+    }
+}
+
 export class VariableResultSettings {
     private visible: boolean;
     private legendVisible: boolean;
     private legendSubscribers: ((legendVisible: boolean) => void)[];
     private useSetDataDescriptions: boolean;
     private useSetDataDescriptionsSubscribers: ((useSetDataDescriptions: boolean) => void)[];
-    private setDataOptions: ISelectableOption[];
+    private setDataGroups: SetDataGroup[];
     private setDataFilterSubscribers: ((selectedDataValues: string[]) => void)[];
 
     constructor() {
@@ -43,14 +72,16 @@ export class VariableResultSettings {
     }
 
     updateSetData = (setData: ISetData) => {
-        this.setDataOptions = _.map(setData.items, i => {
-            return {
-                value: i.value,
-                name: i.name,
-                selected: true,
-                display: i.value == i.name ? i.value : `${i.value} (${i.name})`,
-            };
+        var groups = setData && setData.groups ? setData.groups : [];
+        var items = setData && setData.items ? setData.items : [];
+        var sortedGroups = _.sortBy(groups, g => g.name);
+        var groupList = _.map(sortedGroups, g => {
+            var groupSetData = _.filter(items, i => i.groupId == g.id);
+            return new SetDataGroup(g.id, g.name, groupSetData, this.notifySetDataFilterSubscribers);
         });
+        var ungroupedItems = _.filter(items, c => c.groupId == null);
+        groupList.push(new SetDataGroup(null, 'Ungrouped', ungroupedItems, this.notifySetDataFilterSubscribers));
+        this.setDataGroups = groupList;
         this.notifySetDataFilterSubscribers();
     }
 
@@ -58,28 +89,25 @@ export class VariableResultSettings {
         this.setDataFilterSubscribers.push(subscriber);
     }
 
-    toggleSetData = ($event, setDataOption: ISelectableOption) => {
+    toggleSetData = ($event, setDataOption: ISelectableEntry<ISetDataItem>) => {
         var checkbox = <ICheckbox>$event.target;
         setDataOption.selected = checkbox.checked;
         this.notifySetDataFilterSubscribers();
     }
 
     private notifySetDataFilterSubscribers = () => {
-        var selectedSetDataOptions = _.filter(this.setDataOptions, sdo => sdo.selected);
+        var selectedSetDataOptions = _.flatten(_.map(this.setDataGroups, g => g.selectedItems()));
         var selectedSetDataValues = _.map(selectedSetDataOptions, sdo => sdo.value);
         _.forEach(this.setDataFilterSubscribers, subscriber => {
             subscriber(selectedSetDataValues);
         });
     }
+
+    public displayForNameValue = (name: string, value: string) => {
+        return value == name ? value : `${value} (${name})`;
+    }
 }
 
 interface ICheckbox {
     checked: boolean;
-}
-
-interface ISelectableOption {
-    value: string;
-    name: string;
-    selected: boolean;
-    display: string;
 }
